d036c5b92df1bb31a69bd9816302822e9adec0d0
Alexander Kofman <KOFMAN@il.ibm.com>
2016-01-18 11:47:45 +0200

    respect start offset timestamp in analyzing events

--- src/main/java/org/speedd/perf/Stats.java
+++ src/main/java/org/speedd/perf/Stats.java
@@ -19,27 +19,103 @@ import org.speedd.data.Event;
 import org.speedd.kafka.JsonEventDecoder;
 
 public class Stats {
-	public static final String USAGE = "USAGE: stats -p <percentile> [-f <file>]";
+	public static final String USAGE = "USAGE: stats -p <percentile> [-s <start offset timestamp>] [-f <file>]";
 	private static final String OPTION_PERCENTILE = "p";
 	private static final String OPTION_IN_FILE = "f";
+	private static final String OPTION_START_OFFSET = "s";
 	
 	private ArrayList<Long> latencies;
 	
+	private ArrayList<Long> inEventDelays;
+	
+	private long earliestInEventTimestamp;
+	
+	private long latestInEventTimestamp;
+	
+	private long numOfInEvents;
+	
+	private double avgInEventRate;
+	
+	private long startTimestamp;
+	
+	
 	protected Stats(){
-		
+		earliestInEventTimestamp = 0;
+		latestInEventTimestamp = 0;
+		numOfInEvents = 0;
+		avgInEventRate = 0;
+		startTimestamp = 0;
 	}
 	
-	public static Stats analyze(InputStream eventStream) throws IOException {
+	public static Stats analyze(InputStream eventStream, long startTimestamp) throws IOException {
 		Stats stats = new Stats();
+		
+		stats.setStartTimestamp(startTimestamp);
 		
 		stats.computeLatencies(eventStream);
 		return stats;
 	}
 	
+	protected void setStartTimestamp(long start){
+		startTimestamp = start;
+	}
+	
+	protected long getStartTimestamp(){
+		return startTimestamp;
+	}
+	
 	public long getLatency(float percentile){
-		int percentileIndex = (int) (long) Math.round(latencies.size() * percentile);
+		int percentileIndex = (int) (long) Math.round(latencies.size() * percentile)-1;
 		
 		return latencies.get(percentileIndex);
+	}
+	
+	public double getAvgInRate(){
+		return 1000 * (numOfInEvents - 1) / (latestInEventTimestamp - earliestInEventTimestamp);
+	}
+	
+	private void updateLatencies(Event event){
+		long eventTimestamp = event.getTimestamp();
+
+		Object[] timestamps = (Object[])event.getAttributes().get("timestamps");
+		
+		Long[] ts = new Long[timestamps.length];
+		
+		if(timestamps instanceof String[]){
+			String[] strTS = (String[])timestamps;
+			
+			for (int i=0; i<strTS.length; ++i) {
+				ts[i] = Long.valueOf(strTS[i]);
+			}
+		} else {
+			ts = (Long[])timestamps;
+		}
+
+		Arrays.sort(ts);
+		
+		Long latestInTS = ts[ts.length-1];
+		Long latency = eventTimestamp - latestInTS;
+		
+		latencies.add(latency);
+	}
+
+	private void updateInEventMetrics(Event event){
+		long timestamp = event.getTimestamp();
+		
+		numOfInEvents++;
+		
+		if(earliestInEventTimestamp == 0){
+			earliestInEventTimestamp = timestamp;
+			latestInEventTimestamp = timestamp;
+		} else {
+			if(earliestInEventTimestamp > timestamp){
+				earliestInEventTimestamp = timestamp;
+			}
+			
+			if(latestInEventTimestamp < timestamp){
+				latestInEventTimestamp = timestamp;
+			}
+		}
 	}
 	
 	protected void computeLatencies(InputStream eventStream) throws IOException {
@@ -49,8 +125,6 @@ public class Stats {
 		
 		latencies = new ArrayList<Long>();
 		
-		int nValues = 0;
-
 		JsonEventDecoder decoder = new JsonEventDecoder();
 
 		do {
@@ -62,32 +136,28 @@ public class Stats {
 			
 			Event event = decoder.fromBytes(line.getBytes());
 			
+			//skip events earlier than the start offset timestamp
+			if(event.getTimestamp() < startTimestamp){
+				continue;
+			}
+			
 			long eventTimestamp = event.getTimestamp();
 			
-			Object[] timestamps = (Object[])event.getAttributes().get("timestamps");
-			
-			Long[] ts = new Long[timestamps.length];
-			
-			if(timestamps instanceof String[]){
-				String[] strTS = (String[])timestamps;
-				
-				for (int i=0; i<strTS.length; ++i) {
-					ts[i] = Long.valueOf(strTS[i]);
-				}
+			if(event.getAttributes().containsKey("timestamps")){
+				updateLatencies(event);
 			} else {
-				ts = (Long[])timestamps;
+				//input event - does not contain 'timestamps' - use to compute real rates
+				updateInEventMetrics(event);
 			}
-
-			Arrays.sort(ts);
 			
-			Long latestInTS = ts[ts.length-1];
-			Long latency = eventTimestamp - latestInTS;
-			
-			latencies.add(latency);
 		} while (!done);
 		
 		Collections.sort(latencies);
 		
+	}
+	
+	public long getNumOfInEvents(){
+		return numOfInEvents;
 	}
 	
 	public static void main(String[] args) {
@@ -95,6 +165,7 @@ public class Stats {
 
 		options.addOption(Option.builder(OPTION_PERCENTILE).hasArg().type(Float.class).required().build());
 		options.addOption(Option.builder(OPTION_IN_FILE).hasArg().build());
+		options.addOption(Option.builder(OPTION_START_OFFSET).required(false).hasArg().build());
 
 		try {
 			CommandLineParser clParser = new DefaultParser();
@@ -106,6 +177,8 @@ public class Stats {
 				throw new ParseException("Percentile value must be within (0,1]");
 			}
 			
+			long startOffset = cmd.hasOption(OPTION_START_OFFSET)? Long.valueOf(cmd.getOptionValue(OPTION_START_OFFSET)) : 0;
+			
 			InputStream in = null;
 			
 			if(cmd.hasOption(OPTION_IN_FILE)){
@@ -115,10 +188,10 @@ public class Stats {
 				in = System.in;
 			}
 			
-			Stats stats = Stats.analyze(in);
+			Stats stats = Stats.analyze(in, startOffset);
 			
 			System.out.println(String.format("%.1f%% latency: %d ms", percentile, stats.getLatency(percentile)));
-
+			System.out.println(String.format("Num of input events: %d, average rate: %f events/sec", stats.numOfInEvents, stats.getAvgInRate() ));
 			
 		} catch (ParseException pe){
 			System.err.println(pe.getMessage());
--- src/test/java/org/speedd/perf/StatsTest.java
+++ src/test/java/org/speedd/perf/StatsTest.java
@@ -1,18 +1,39 @@
 package org.speedd.perf;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.FileInputStream;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class StatsTest {
 	@Test
-	public void latencyTest() throws Exception {
-		Stats stats = Stats.analyze(new FileInputStream(getClass().getClassLoader().getResource("out-decisions").getFile()));
+	public void statsTest() throws Exception {
+		Stats stats = Stats.analyze(new FileInputStream(getClass().getClassLoader().getResource("out-decisions").getFile()), 0);
 
 		assertNotNull(stats);
 		
-		assertEquals(101, stats.getLatency(0.9f));
+		assertEquals(8, stats.getLatency(0.9f));
+		
+		assertEquals(48, stats.getNumOfInEvents());
+		
+		assertEquals(3.0, stats.getAvgInRate(), 1E-4);
+	}
+	
+	@Test
+	public void statsTestWithOffset() throws Exception {
+		long offset = 1452884299500L;
+		
+		Stats stats = Stats.analyze(new FileInputStream(getClass().getClassLoader().getResource("out-decisions").getFile()), offset);
+
+		assertNotNull(stats);
+		
+		assertEquals(9, stats.getLatency(0.9f));
+		
+		assertEquals(41, stats.getNumOfInEvents());
+		
+		assertEquals(3.0, stats.getAvgInRate(), 1E-4);
 	}
 
 }
