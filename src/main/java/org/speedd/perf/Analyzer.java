e763f259677685ead9ba778ef02a8cbefaae23b2
Alexander Kofman <KOFMAN@il.ibm.com>
2016-01-23 17:23:43 +0200

    initial code for analyzer

---
+++ src/main/java/org/speedd/perf/Analyzer.java
@@ -0,0 +1,131 @@
+package org.speedd.perf;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.SimpleLoggerFactory;
+
+public class Analyzer {
+	private static final String OPTION_ZK_CONNECT = "zookeeper";
+	private static final String OPTION_TOPICS = "topics";
+	private static final String OPTION_THREADS_PER_TOPIC = "threads";
+	private static final String USAGE = "analyzer --zookeeper=<host:port> --group=<group id> --topics=<name1,name2,...> --threads=<number>";
+	private static final String OPTION_GROUP_ID = "group";
+	private ConsumerConnector consumer;
+	private String[] topics;
+	private int threadsPerTopic;
+	private ExecutorService executor;
+	
+	private static class EventRecorder implements Runnable {
+		private KafkaStream stream;
+		private int threadId;
+		private String topic;
+		
+		public EventRecorder(String topic, KafkaStream stream, int threadId) {
+			this.stream = stream;
+			this.threadId = threadId;
+			this.topic = topic;
+		}
+
+		@Override
+		public void run() {
+			System.out.println("Starting thread " + threadId + " for topic " + topic);
+			
+	        ConsumerIterator<byte[], byte[]> it = stream.iterator();
+	        while (it.hasNext()) {
+	        	long timestamp = System.currentTimeMillis();
+	        	String message = new String(it.next().message());
+	        	System.out.println(timestamp + " : " + message);
+	        }
+	            
+	        System.out.println("Shutting down thread: " + threadId + " for topic " + topic);
+		}
+		
+	}
+	
+	public Analyzer(String zkConnect, String[] topics, String groupId, int threadsPerTopic){
+		consumer = Consumer.createJavaConsumerConnector(createConsumerConfig(zkConnect, groupId));
+		this.topics = topics;
+		this.threadsPerTopic = threadsPerTopic;
+	}
+	
+    private static ConsumerConfig createConsumerConfig(String zkConnect, String groupId) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", zkConnect);
+        props.put("group.id", groupId);
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("zookeeper.sync.time.ms", "200");
+        props.put("auto.commit.interval.ms", "1000");
+ 
+        return new ConsumerConfig(props);
+    }
+    
+    public void run() {
+    	Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+    	
+    	for (String topic : topics) {
+    		topicCountMap.put(topic, new Integer(threadsPerTopic));
+    	}
+    	
+    	Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+
+        executor = Executors.newFixedThreadPool(topics.length * threadsPerTopic);
+
+    	for (String topic : topics) {
+            List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+            
+            int threadId = 0;
+            
+            for (KafkaStream<byte[], byte[]> stream : streams) {
+				executor.submit(new EventRecorder(topic, stream, threadId));
+				threadId++;
+			}
+		}
+ 
+    }
+    
+	public static void main(String[] args) {
+		Options options = new Options();
+
+		options.addOption(Option.builder().longOpt(OPTION_ZK_CONNECT).hasArg().required().build());
+		options.addOption(Option.builder().longOpt(OPTION_GROUP_ID).hasArg().required().build());
+		options.addOption(Option.builder().longOpt(OPTION_TOPICS).hasArg().build());
+		options.addOption(Option.builder().longOpt(OPTION_THREADS_PER_TOPIC).required().type(Integer.class).hasArg().build());
+
+		try {
+			CommandLineParser clParser = new DefaultParser();
+			CommandLine cmd = clParser.parse(options, args);
+			
+			String zkConnect = cmd.getOptionValue(OPTION_ZK_CONNECT);
+			String groupId = cmd.getOptionValue(OPTION_GROUP_ID);
+			String[] topics = cmd.getOptionValue(OPTION_TOPICS).split("\\s*,\\s*");
+			int threadsPerTopic = Integer.valueOf(cmd.getOptionValue(OPTION_THREADS_PER_TOPIC));
+			
+			Analyzer analyzer = new Analyzer(zkConnect, topics, groupId, threadsPerTopic);
+			analyzer.run();
+
+		} catch (ParseException e){
+			System.err.println(USAGE);
+		}
+	}
+
+	
+}
